c99de6fb760bfd80dd09f4784d8a8b8498056a90
AlanT <alan.tejeda.te@gmail.com>
2026-03-13 13:34:37 -0600

    fopen

--- src/fhndlr.c
+++ src/fhndlr.c
@@ -1,17 +1,67 @@
 #include "fhndlr.h"
+#include <stdio.h>
 
-char* lowercase(char* input)
+char* lowercase(char* string)
 {
-    return input;
+    int i = 0;
+    while (string[i] != '\0')
+    {
+        if (string[i] >= 'A' && string[i] <='Z')
+        {
+            string[i] += ('a' - 'A');
+        }
+        i++;
+    }
+    return string;
 }
 
-
-char* uppercase(char* input)
+    
+char* uppercase(char* string)
 {
-    return input;
+    int i = 0;
+    while (string[i] != '\0')
+    {
+        if (string[i] >= 'a' && string[i] <= 'z')
+        {
+            string[i] -= ('a' - 'A');
+        }
+        i++;
+    }
+    return string;
 }
 
-char* capitalize(char* input)
-{
+char* capitalize(char* input) {
+    if (!input) return input;
+    
+    char* ptr = input;
+    int new_word = 1; 
+
+    while (*ptr != '\0') 
+    {
+        if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z')) 
+        {
+            if (new_word) 
+            {
+            
+                if (*ptr >= 'a' && *ptr <= 'z') 
+                {
+                    *ptr = *ptr - 32;
+                }
+                new_word = 0;
+            } else 
+            {
+            
+                if (*ptr >= 'A' && *ptr <= 'Z') 
+                {
+                    *ptr = *ptr + 32;
+                }
+            }
+        } else 
+        {
+        
+            new_word = 1;
+        }
+        ptr++;
+    }
     return input;
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "fhndlr.h"
+
+void copy_string(char* dest, const char* src) 
+{
+    while (*src != '\0') 
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+}
+
+#define BUFFER_SIZE 8192
 
 int main() 
 {
-    printf("Hello World!\n");
+
+    FILE *file = fopen("../inputs/lorem.txt", "r");
+    if (file == NULL) 
+    {
+        printf("Error: No se pudo abrir ../inputs/lorem.txt\n");
+        return 1; 
+    }
+
+    char original_text[BUFFER_SIZE];
+    size_t bytes_read = fread(original_text, 1, BUFFER_SIZE - 1, file);
+    original_text[bytes_read] = '\0'; 
+    fclose(file);
+
+
+    char work_buffer[BUFFER_SIZE];
+
+    copy_string(work_buffer, original_text);
+    lowercase(work_buffer);
+
+    FILE *f_lower = fopen("../build/out/lower.txt", "w");
+    if (f_lower) 
+    {
+        fputs(work_buffer, f_lower);
+        fclose(f_lower);
+    }
+
+
+    copy_string(work_buffer, original_text);
+    uppercase(work_buffer);
+
+    FILE *f_upper = fopen("../build/out/upper.txt", "w");
+    if (f_upper) 
+    {
+        fputs(work_buffer, f_upper);
+        fclose(f_upper);
+    }
+
+    copy_string(work_buffer, original_text);
+    capitalize(work_buffer);
+    
+    FILE *f_cap = fopen("../build/out/capitalize.txt", "w");
+    if (f_cap) 
+    {
+        fputs(work_buffer, f_cap);
+        fclose(f_cap);
+    }
+
+    printf("Archivos generados correctamente para el autograder.\n");
     return 0;
 }
